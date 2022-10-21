1e93b2ebfadc5733571e16a34359bbbbe0dcb06d
Krishan Bhatt <krishan.bhatt@hcl.com>
2022-10-21 17:35:57 +0530

    done

--- shebeng.sh
+++ shebeng.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-echo hello world
-echo test
-echo test1
-echo test2
-echo test3
-echo test4
+#echo hello world
+#echo test
+#echo test1
+#echo test2
+#echo test3
+#echo test4
+ACTION=$1
+case $ACTION in 
+    start)
+        echo"start"
+        exit 0
+        ;;
+    start)
+        echo"start"
+        exit 0
+        ;;
+esac    
+
