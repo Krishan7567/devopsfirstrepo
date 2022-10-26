fcb292e5afe5e35830357ff183f7d98f5f6a8123
Krishan Bhatt <krishan.bhatt@hcl.com>
2022-10-26 15:24:57 +0530

    done

---
+++ robot/component/frontend.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+set -e 
+
+COMPONENT=frontend
+
+source components/common.sh
+
+echo -n "Installing Ngnix:"
+yum install nginx -y &>> $LOGFILE
+stat $?
+
+echo -n "Downloading the component: "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+
+echo -n "Performing Cleanup:"
+rm -rf /usr/share/nginx/html/*  &>> $LOGFILE
+stat $?
+
+cd /usr/share/nginx/html
+echo -n "Unzipping the $COMPONENT:"
+unzip /tmp/$COMPONENT.zip &>> $LOGFILE
+stat $? 
+mv $COMPONENT-main/* .
+mv static/* .
+rm -rf $COMPONENT-main README.md &>> $LOGFILE
+
+echo -n "Conifuring the reverse proxy file:"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -n "Starting Frontend Service:"
+systemctl enable nginx &>> $LOGFILE
+systemctl start nginx &>> $LOGFILE
+stat $?
+
+
